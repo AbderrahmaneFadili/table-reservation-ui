bd8422303d189ee1f615b76346ae66f0f7ef0ac2
Abderrahmane Fadili <abdouelrfadili@gmail.com>
2022-07-01 16:35:27 +0100

    Increment button & Decrement buttons using JS

--- main.js
+++ main.js
@@ -1,1 +1,21 @@
-console.log("table reservation ui");
+/* ===== get elements  =====  */
+const btnIncrement = document.querySelector("#btn__increment");
+const btnDecrement = document.querySelector("#btn__decrement");
+const quantityNumber = document.querySelector(".quantity__number");
+
+btnIncrement.addEventListener("click", (event) => {
+  let personsNumber = parseInt(quantityNumber.textContent);
+  if (personsNumber < 4) {
+    personsNumber++;
+    quantityNumber.textContent = personsNumber;
+  }
+});
+
+btnDecrement.addEventListener("click", (event) => {
+  let personsNumber = parseInt(quantityNumber.textContent);
+
+  if (personsNumber > 0) {
+    personsNumber--;
+    quantityNumber.textContent = personsNumber;
+  }
+});
